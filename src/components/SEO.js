50e35218cec25de9edccabf0b22723e4962993b3
Collins Muriuki <murerwacollins@gmail.com>
2020-05-11 02:02:15 +0300

    Improve SEO component

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import { Helmet } from "react-helmet";
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
+
+const getMetadata = graphql`
+  {
+    site {
+      siteMetadata {
+        siteTitle: title
+        siteDescription: description
+        author
+      }
+    }
+  }
+`
 
 const SEO = ({ title, description }) => {
-  return (
-    <Helmet htmlAttributes={{ lang: "en" }} title={title}>
-      <meta name="description" content={description} />
-    </Helmet>
-  );
-};
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(getMetadata)
+  const { siteTitle, siteDescription, author } = siteMetadata
 
-export default SEO;
+  return (
+    <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
+      <meta name="description" content={description || siteDescription} />
+      <meta name="author" content={author} />
+    </Helmet>
+  )
+}
+
+export default SEO
